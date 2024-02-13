25adc0b3c5dfa42053fd0b97189bcc9a717b8462
sudhanshu panthri <panthrisudhanshu666@gmail.com>
2024-02-13 14:22:38 +0530

    Added Login and Register View Models

--- Models/RegisterViewModel.cs
+++ Models/RegisterViewModel.cs
@@ -1,7 +1,20 @@
-﻿namespace Assignment__1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Assignment__1.Models
 {
     public class RegisterViewModel
     {
-        public string Name { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string? Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+        [Compare("Password",ErrorMessage ="Password Does not match")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string? ConfirmPassword { get; set; }
     }
 }
