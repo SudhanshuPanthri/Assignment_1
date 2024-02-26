c207f8e77188f1911f8226fa857a4ab706267812
sudhanshu panthri <panthrisudhanshu666@gmail.com>
2024-02-27 01:39:37 +0530

    UI Changes

--- obj/Debug/net8.0/Assignment__1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Assignment__1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment__1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5d2fe6a215f29db7616b381961882a30c404b20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31f284a943ce033083bd446e0d40111e3a07aeca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment__1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment__1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
